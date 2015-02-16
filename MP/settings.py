0dd40d88997edd6ee9d97e1247ee9d22c73a641c
gzacur <guillermo.zacur@gmail.com>
2015-02-16 18:45:36 -0300

    Nuevo directorio de configuraciones

--- MP/settings.py
+++ MP/settings.py
@@ -61,19 +61,19 @@ ROOT_URLCONF = 'MP.urls'
 WSGI_APPLICATION = 'MP.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mp_inscripciones',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# # Database
+# # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'mp_inscripciones',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
